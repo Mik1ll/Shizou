9d05a523474dc9b6fdb0c1f7c578e5ea65af5c09
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2021-06-14 04:48:08 -0700

    replace ActivatorUtilties calls with invoke

--- Shizou/Commands/CommandManager.cs
+++ Shizou/Commands/CommandManager.cs
@@ -11,13 +11,17 @@ namespace Shizou.Commands
 {
     public class CommandManager
     {
-        public static readonly Dictionary<CommandType, (Type type, Type paramType)> Commands = Assembly
+        public static readonly List<(CommandType cmdType, Type type, Type paramType, ConstructorInfo ctor)> Commands = Assembly
             .GetExecutingAssembly().GetTypes()
             .Select(t => new {type = t, commandAttr = t.GetCustomAttribute<CommandAttribute>()})
             .Where(x => x.commandAttr is not null)
-            .ToDictionary(x => x.commandAttr!.Type, x => (x.type, x.type.BaseType!.GetGenericArguments()[0]));
-
-        public static readonly Dictionary<Type, Type> TypeFromParam = Commands.ToDictionary(x => x.Value.paramType, x => x.Value.type);
+            .Select(x => (
+                x.commandAttr!.Type,
+                x.type,
+                x.type.BaseType!.GetGenericArguments()[0],
+                x.type.GetConstructor(BindingFlags.Instance | BindingFlags.Public, null, CallingConventions.HasThis,
+                    new[] {typeof(IServiceProvider), typeof(CommandParams)}, null)!))
+            .ToList();
 
         private readonly IServiceProvider _serviceProvider;
 
@@ -26,20 +30,20 @@ namespace Shizou.Commands
             _serviceProvider = serviceProvider;
         }
 
-        // TODO: Get ctors from dictionary, don't use ActivatorUtilities
+        // TODO: Test new implementation without activatorutils
         public void Dispatch<TParams>(TParams commandParams)
             where TParams : CommandParams
         {
             using var context = new ShizouContext();
-            context.CommandRequests.Add(((ICommand)ActivatorUtilities.CreateInstance(_serviceProvider, TypeFromParam[commandParams.GetType()], commandParams))
-                .CommandRequest);
+            var command = Commands.First(x => commandParams.GetType() == x.paramType);
+            context.CommandRequests.Add(((ICommand)command.ctor.Invoke(new object[] {_serviceProvider, commandParams})).CommandRequest);
             context.SaveChanges();
         }
 
         public ICommand CommandFromRequest(CommandRequest commandRequest)
         {
-            var (type, paramType) = Commands[commandRequest.Type];
-            return (ICommand)ActivatorUtilities.CreateInstance(_serviceProvider, type, JsonSerializer.Deserialize(commandRequest.CommandParams, paramType)!);
+            var command = Commands.First(x => commandRequest.Type == x.cmdType);
+            return (ICommand)command.ctor.Invoke(new [] {_serviceProvider, JsonSerializer.Deserialize(commandRequest.CommandParams, command.paramType)!});
         }
     }
 }
