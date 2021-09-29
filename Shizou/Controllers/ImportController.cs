865c6b835bc9140c5a930b54afd1927c93604b18
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2021-09-28 17:39:23 -0700

    add import controller

---
+++ Shizou/Controllers/ImportController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shizou.Services.Import;
+
+namespace Shizou.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImportController : ControllerBase
+    {
+        private readonly ILogger<ImportController> _logger;
+        private readonly Importer _importer;
+
+        public ImportController(ILogger<ImportController> logger, Importer importer)
+        {
+            _logger = logger;
+            _importer = importer;
+        }
+
+        /// <summary>
+        ///     Start import
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut("start")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult StartImport()
+        {
+            _importer.Import();
+            return Ok();
+        }
+
+        /// <summary>
+        ///     Scan an import folder
+        /// </summary>
+        /// <param name="folderId"></param>
+        /// <returns></returns>
+        [HttpPut("scan/{folderId}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult ScanFolder(int folderId)
+        {
+            _importer.ScanImportFolder(folderId);
+            return Ok();
+        }
+    }
+}
