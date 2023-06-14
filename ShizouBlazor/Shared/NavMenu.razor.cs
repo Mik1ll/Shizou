761837e3fd213fe4027a42de7e4ff2f1723aa877
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-06-13 21:59:13 -0700

    Seperate codebehind

---
+++ ShizouBlazor/Shared/NavMenu.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace ShizouBlazor.Shared;
+
+public partial class NavMenu
+{
+    private bool _collapseNavMenu = true;
+
+    private string? NavMenuCssClass => _collapseNavMenu ? "collapse" : null;
+
+    private void ToggleNavMenu()
+    {
+        _collapseNavMenu = !_collapseNavMenu;
+    }
+}
---
+++ ShizouBlazor/Shared/Queues.razor.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Shizou.CommandProcessors;
+using ShizouData.Database;
+
+namespace ShizouBlazor.Shared;
+
+public partial class Queues : IDisposable
+{
+    private readonly List<CommandProcessor> _processors = new();
+
+    [Inject]
+    private IDbContextFactory<ShizouContext> ContextFactory { get; set; } = default!;
+
+    [Inject]
+    private ILogger<Queues> Logger { get; set; } = default!;
+
+    [Inject]
+    private IServiceProvider ServiceProvider { get; set; } = default!;
+
+    public void Dispose()
+    {
+        foreach (var processor in _processors)
+            processor.PropertyChanged -= OnCommandChanged;
+    }
+
+    protected override void OnInitialized()
+    {
+        _processors.AddRange(ServiceProvider.GetServices<CommandProcessor>());
+        foreach (var processor in _processors)
+            processor.PropertyChanged += OnCommandChanged;
+    }
+
+    private void OnCommandChanged(object? sender, PropertyChangedEventArgs eventArgs)
+    {
+        InvokeAsync(StateHasChanged);
+    }
+}
