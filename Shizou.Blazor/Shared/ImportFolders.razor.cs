9a9d70a7aeaf85572c46cf91ba0f4c90e1662d22
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-06-26 04:31:42 -0700

    Add scan tooltip and force scan

---
+++ Shizou.Blazor/Shared/ImportFolders.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Shizou.Blazor.Shared;
+
+public partial class ImportFolders
+{
+    [Inject]
+    private IJSRuntime JsRuntime { get; set; } = default!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await JsRuntime.InvokeVoidAsync("loadTooltip", "scanButton");
+        await base.OnAfterRenderAsync(firstRender);
+    }
+}
