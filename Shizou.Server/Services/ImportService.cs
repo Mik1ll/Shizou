1828be468289b9e6b8e84924e638ea39727d5d13
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-07-03 20:39:30 -0700

    Simplify filtering files to hash in import

--- Shizou.Server/Services/ImportService.cs
+++ Shizou.Server/Services/ImportService.cs
@@ -46,24 +46,10 @@ public class ImportService
         _logger.LogInformation("Beginning scan on import folder \"{ImportFolderPath}\"", importFolder.Path);
         var dir = new DirectoryInfo(importFolder.Path);
         var allFiles = dir.GetFiles("*", SearchOption.AllDirectories);
-        var filesToHash = allFiles
-            // Left Outer Join on all files with DB local files. Includes DB files that are not ignored and mismatch filesize.
-            .GroupJoin(
-                context.LocalFiles
-                    .Join(context.ImportFolders,
-                        e => e.ImportFolderId,
-                        e => e.Id,
-                        (file, folder) => new
-                        {
-                            Path = Path.GetFullPath(Path.Combine(folder.Path, file.PathTail)),
-                            file.FileSize,
-                            file.Ignored
-                        }),
-                e => e.FullName,
-                e => e.Path,
-                (info, dbLocals) => new { FileInfo = info, LocalFile = dbLocals.FirstOrDefault() })
-            .Where(e => !(e.LocalFile?.Ignored ?? false) && (e.FileInfo.Length != e.LocalFile?.FileSize || forceRescan))
-            .Select(e => e.FileInfo);
+        var dbFiles = context.LocalFiles.Include(lf => lf.ImportFolder)
+            .ToDictionary(lf => Path.Combine(lf.ImportFolder.Path, lf.PathTail));
+        var filesToHash = allFiles.Where(f =>
+            !dbFiles.TryGetValue(f.FullName, out var lf) || (!lf.Ignored && (forceRescan || f.Length != lf.FileSize)));
 
         _commandService.DispatchRange(filesToHash.Select(e => new HashArgs(e.FullName)));
     }
