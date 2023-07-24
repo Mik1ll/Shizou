490a44e81c15b10a781685fdfaddd2a5aba9d720
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-07-24 01:08:13 -0700

    Implement MyListEntryRequest.cs

--- Shizou.Server/AniDbApi/Requests/Udp/MyListEntryRequest.cs
+++ Shizou.Server/AniDbApi/Requests/Udp/MyListEntryRequest.cs
@@ -1,17 +1,44 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using Shizou.Data.Enums;
 
 namespace Shizou.Server.AniDbApi.Requests.Udp;
 
 public class MyListEntryRequest : AniDbUdpRequest
 {
+    public MyListEntryResult? MyListEntryResult { get; private set; }
+
     public MyListEntryRequest(ILogger<MyListEntryRequest> logger, AniDbUdpState aniDbUdpState) : base("MYLIST", logger, aniDbUdpState)
     {
     }
 
     protected override Task HandleResponse()
     {
-        throw new NotImplementedException();
+        switch (ResponseCode)
+        {
+            case AniDbResponseCode.MyList:
+                if (string.IsNullOrWhiteSpace(ResponseText))
+                    return Task.CompletedTask;
+                var data = ResponseText.Split('|');
+                MyListEntryResult = new MyListEntryResult(int.Parse(data[0]),
+                    int.Parse(data[1]),
+                    int.Parse(data[2]),
+                    int.Parse(data[3]),
+                    int.Parse(data[4]),
+                    DateTimeOffset.FromUnixTimeSeconds(long.Parse(data[5])),
+                    Enum.Parse<MyListState>(data[6]),
+                    DateTimeOffset.FromUnixTimeSeconds(long.Parse(data[7])),
+                    data[8],
+                    data[9],
+                    data[10],
+                    Enum.Parse<MyListFileState>(data[11]));
+                break;
+            case AniDbResponseCode.MultipleMyListEntries:
+                break;
+            case AniDbResponseCode.NoSuchEntry:
+                break;
+        }
+        return Task.CompletedTask;
     }
 }
---
+++ Shizou.Server/AniDbApi/Requests/Udp/MyListEntryResult.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using Shizou.Data.Enums;
+
+namespace Shizou.Server.AniDbApi.Requests.Udp;
+
+public record MyListEntryResult(int MyListId, int FileId, int EpisodeId, int AnimeId, int GroupId, DateTimeOffset AddedDate, MyListState State,
+    DateTimeOffset ViewedDate, string Storage, string Source, string Other, MyListFileState FileState);
