c8a76ee4601694601f4c229fbd06bd079c077eda
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-08-02 01:50:51 -0700

    Don't query db on render, use new on modal closed method

--- Shizou.Blazor/Shared/ImportFolders.razor.cs
+++ Shizou.Blazor/Shared/ImportFolders.razor.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.JSInterop;
+using Shizou.Data.Database;
+using Shizou.Data.Models;
+using Shizou.Server.Services;
 
 namespace Shizou.Blazor.Shared;
 
 public partial class ImportFolders
 {
+    private ImportFolderModal? _importFolderModal;
+    private List<ImportFolder> _importFolders = default!;
+
     [Inject]
     private IJSRuntime JsRuntime { get; set; } = default!;
+
+    [Inject]
+    private IDbContextFactory<ShizouContext> ContextFactory { get; set; } = default!;
+
+    [Inject]
+    private ImportService ImportService { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        _importFolders = ContextFactory.CreateDbContext().ImportFolders.ToList();
+    }
+
+    private void ImportFolderModalClosed()
+    {
+        _importFolders = ContextFactory.CreateDbContext().ImportFolders.ToList();
+        StateHasChanged();
+    }
 
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
