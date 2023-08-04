19920e0264ff3ca3c278e1cc17c75e41d23d03f6
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-08-04 03:13:55 -0700

    Webpaths static

--- Shizou.Blazor/WebPaths.cs
+++ Shizou.Blazor/WebPaths.cs
@@ -2,7 +2,7 @@
 
 namespace Shizou.Blazor;
 
-public class WebPaths
+public static class WebPaths
 {
     public static readonly string ImagesDir = $"/{Path.GetFileName(FilePaths.ImagesDir)}";
     public static readonly string AnimePostersDir = $"{ImagesDir}/{Path.GetRelativePath(FilePaths.ImagesDir, FilePaths.AnimePostersDir)
