206583ecaf365c82157cca99a6fbb7193b7283e2
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-08-04 20:45:57 -0700

    Cleaner webpaths declaration

--- Shizou.Blazor/WebPaths.cs
+++ Shizou.Blazor/WebPaths.cs
@@ -4,7 +4,12 @@ namespace Shizou.Blazor;
 
 public static class WebPaths
 {
-    public static readonly string ImagesDir = $"/{Path.GetFileName(FilePaths.ImagesDir)}";
-    public static readonly string AnimePostersDir = $"{ImagesDir}/{Path.GetRelativePath(FilePaths.ImagesDir, FilePaths.AnimePostersDir)
-        .Replace('\\', '/')}";
+    public static readonly string ImagesDir = GetWebPath(FilePaths.ImagesDir);
+    public static readonly string AnimePostersDir = GetWebPath(FilePaths.AnimePostersDir);
+
+    public static string GetWebPath(string filepath)
+    {
+        return $"/{Path.GetRelativePath(FilePaths.ApplicationDataDir, filepath)
+            .Replace('\\', '/')}";
+    }
 }
