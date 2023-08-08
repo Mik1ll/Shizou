6142853bcc6a77934768019f3fe2d4e2e38eb315
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-08-08 04:01:40 -0700

    Add file data to anime page

--- Shizou.Blazor/Pages/Anime.razor.cs
+++ Shizou.Blazor/Pages/Anime.razor.cs
@@ -23,6 +23,8 @@ public partial class Anime
     private readonly Regex _splitRegex = new(@"(?<=https?:\/\/\S*? \[.*?\])|(?=https?:\/\/\S*? \[.*?\])", RegexOptions.Compiled);
     private readonly Regex _matchRegex = new(@"(https?:\/\/\S*?) \[(.*?)\]", RegexOptions.Compiled);
 
+    private readonly Dictionary<int, bool> _episodeExpanded = new();
+
     protected override void OnInitialized()
     {
         using var context = ContextFactory.CreateDbContext();
@@ -33,5 +35,34 @@ public partial class Anime
     {
         if (WatchStateService.MarkEpisode(ep.Id, watched))
             ep.Watched = watched;
+    }
+
+    private void MarkFile(AniDbFile f, bool watched)
+    {
+        if (WatchStateService.MarkFile(f.Id, watched))
+            f.Watched = watched;
+    }
+
+    private void ToggleEpExpand(AniDbEpisode ep)
+    {
+        if (_episodeExpanded.TryGetValue(ep.Id, out var expanded))
+            _episodeExpanded[ep.Id] = !expanded;
+        else
+            _episodeExpanded[ep.Id] = true;
+    }
+
+    private List<LocalFile> GetManualLinksForEpisode(AniDbEpisode ep)
+    {
+        using var context = ContextFactory.CreateDbContext();
+        return context.LocalFiles.Include(lf => lf.ManualLinkXrefs).Where(lf => lf.ManualLinkXrefs.Any(x => x.AniDbEpisodeId == ep.Id)).ToList();
+    }
+
+    private List<(AniDbFile, LocalFile)> GetFilesForEpisode(AniDbEpisode ep)
+    {
+        using var context = ContextFactory.CreateDbContext();
+        return context.FilesFromEpisode(ep.Id).Include(f => f.AniDbGroup)
+            .Join(context.LocalFiles, f => f.Ed2K, lf => lf.Ed2K,
+                (f, lf) => new { f, lf }).ToList()
+            .Select(f => (f.f, f.lf)).ToList();
     }
 }
