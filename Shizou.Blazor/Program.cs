32f3f25d81406594c93810c01e067c2d5d149b2f
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-08-14 04:55:45 -0700

    Add api functionality to blazor project

--- Shizou.Blazor/Program.cs
+++ Shizou.Blazor/Program.cs
@@ -1,9 +1,10 @@
-using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using Serilog;
 using Shizou.Blazor;
 using Shizou.Data;
 using Shizou.Data.Database;
+using Shizou.Server;
 using Shizou.Server.Extensions;
 
 var logTemplate = "{Timestamp:HH:mm:ss} {Level:u3} | {SourceContext} {Message:lj}{NewLine:1}{Exception:1}";
@@ -18,11 +19,13 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
 builder.AddShizouOptions()
+    .AddShizouLogging(logTemplate)
     .AddShizouServices()
     .AddAniDbServices()
     .AddShizouProcessors()
-    .AddShizouLogging(logTemplate);
+    .AddShizouApiServices();
 
+builder.Services.AddHostedService<StartupService>();
 
 var app = builder.Build();
 
@@ -33,6 +36,12 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
@@ -48,7 +57,17 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapControllers().RequireAuthorization();
+
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ShizouContext>();
+    context.Database.Migrate();
+}
 
 app.Run();
