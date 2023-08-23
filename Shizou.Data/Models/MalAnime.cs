b1b9fcc5d57098728f3cc246b7719e8209016c5a
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-08-22 22:34:07 -0700

    Mal Ids are not database generated

--- Shizou.Data/Models/MalAnime.cs
+++ Shizou.Data/Models/MalAnime.cs
@@ -1,7 +1,10 @@
-﻿namespace Shizou.Data.Models;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shizou.Data.Models;
 
 public class MalAnime : IEntity
 {
+    [DatabaseGenerated(DatabaseGeneratedOption.None)]
     public int Id { get; set; }
     public MalStatus? Status { get; set; }
 }
