fd64344aa35ff7481ea13f872fba8f96e91039e7
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-08-22 22:30:37 -0700

    Add test for anime deserialization, add words to dictionary

---
+++ Shizou.Tests/SerializationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shizou.Server.FileCaches;
+
+namespace Shizou.Tests;
+
+[TestClass]
+public class SerializationTests
+{
+    private readonly ServiceProvider _serviceProvider;
+
+    public SerializationTests()
+    {
+        var serviceCollection = new ServiceCollection();
+        serviceCollection.AddScoped<HttpAnimeResultCache>();
+        serviceCollection.AddLogging();
+        _serviceProvider = serviceCollection.BuildServiceProvider();
+    }
+
+    [TestMethod]
+    public void TestAnimeDeserialization()
+    {
+        var cache = _serviceProvider.GetRequiredService<HttpAnimeResultCache>();
+        var animeResult = cache.Get("AnimeDoc_12704.xml").Result;
+        if (animeResult is not null)
+            ;
+        else
+            Assert.Inconclusive("Anime xml not found");
+    }
+}
