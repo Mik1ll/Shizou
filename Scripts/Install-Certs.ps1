d23131e1700205f5432db0a10a9961cea9bb8a8e
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-09-09 04:12:26 -0700

    Fix cert gen and port settings

--- Scripts/Install-Certs.ps1
+++ Scripts/Install-Certs.ps1
@@ -1,12 +1,14 @@
-$cert = New-SelfSignedCertificate -DnsName @("shizou.home", "*.shizou.home") -CertStoreLocation "Cert:\LocalMachine\My"
-$certsPath = Read-Host "Where to save cert files:"
-if (!(Test-Path $certsPath) -or (Get-Item $certsPath) -isnot [System.IO.DirectoryInfo]) {
-    Write-Output "Not a valid directory, aborting"
+#Requires -RunAsAdministrator
+
+$cert = New-SelfSignedCertificate -DnsName @("shizou.home", "*.shizou.home", "localhost") -CertStoreLocation "Cert:\CurrentUser\My"
+$certsPath = (Read-Host "Where to save cert files").Trim('"', "'")
+if (-not (Test-Path $certsPath) -or (Get-Item $certsPath) -isnot [System.IO.DirectoryInfo]) {
+    Write-Output "`"$certsPath`" is not a valid directory, aborting"
     Exit
 }
-$password = ConvertTo-SecureString (Read-Host "Enter a certificate password:") -AsPlainText -Force
+$password = ConvertTo-SecureString (Read-Host "Enter a certificate password") -AsPlainText -Force
 $certPath = Join-Path $certsPath "shizou.home.cer"
-$cert | Export-Certificate -FilePath $certPath
+$cert | Export-Certificate -FilePath $certPath | Out-Null
 $certKeyPath = Join-Path $certsPath "shizou.home.pfx"
-$cert | Export-PfxCertificate -FilePath $certKeyPath -Password $password
-Import-PfxCertificate -FilePath $certKeyPath -CertStoreLocation 'Cert:\LocalMachine\Root' -Password $password
+$cert | Export-PfxCertificate -FilePath $certKeyPath -Password $password | Out-Null
+$null = Import-PfxCertificate -FilePath $certKeyPath -CertStoreLocation 'Cert:\CurrentUser\Root' -Password $password
