346fa75a53de4fd8efe461724813e0cef4a9bdba
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-09-11 05:55:30 -0700

    Save theme preference to local storage, move to js

---
+++ Shizou.Blazor/wwwroot/js/theme.js
@@ -0,0 +1,25 @@
+﻿'use strict';
+const getStoredTheme = () => localStorage.getItem('theme');
+const setStoredTheme = theme => localStorage.setItem('theme', theme);
+const getPreferredTheme = () => {
+    const storedTheme = getStoredTheme();
+    if (storedTheme) {
+        return storedTheme
+    }
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+const setTheme = theme => {
+    if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', theme)
+    }
+}
+window.toggleTheme = () => {
+    const root = document.documentElement;
+    const currentTheme = root.getAttribute("data-bs-theme") || "light";
+    const theme = currentTheme == "light" ? "dark" : "light";
+    setStoredTheme(theme);
+    setTheme(theme);
+}
+setTheme(getPreferredTheme())
