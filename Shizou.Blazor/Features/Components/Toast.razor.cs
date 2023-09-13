eab38d0e35b92fcb8ba89425d20f1356a3b74877
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-09-13 01:41:53 -0700

    Add toast display with cascading parameter

---
+++ Shizou.Blazor/Features/Components/Toast.razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Shizou.Blazor.Features.Components;
+
+public record ToastItem(string Title, string Body, DateTimeOffset TriggerTime);
+
+public partial class Toast
+{
+    private ElementReference _elementReference;
+
+    private bool _isLoaded = false;
+
+    [Parameter]
+    [EditorRequired]
+    public ToastItem ToastItem { get; set; } = default!;
+
+    [Inject]
+    public IJSRuntime JsRuntime { get; set; } = default!;
+
+
+    protected override Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (!_isLoaded)
+        {
+            JsRuntime.InvokeVoidAsync("loadToast", _elementReference);
+            _isLoaded = true;
+        }
+
+        return base.OnAfterRenderAsync(firstRender);
+    }
+}
---
+++ Shizou.Blazor/Features/Components/ToastDisplay.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Shizou.Blazor.Features.Components;
+
+public partial class ToastDisplay
+{
+    private readonly List<ToastItem> _toasts = new();
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    public void AddToast(string title, string body)
+    {
+        _toasts.Add(new ToastItem(title, body, DateTimeOffset.Now));
+        StateHasChanged();
+    }
+}
