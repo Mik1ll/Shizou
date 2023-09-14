8f918fdbf826ff48a41a7f1a357659cecfa00477
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-09-14 04:24:35 -0700

    Split episode table and file cards into components

---
+++ Shizou.Blazor/Features/Anime/Components/EpisodeTable.razor.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Shizou.Data.Database;
+using Shizou.Data.Models;
+using Shizou.Server.Services;
+
+namespace Shizou.Blazor.Features.Anime.Components;
+
+public partial class EpisodeTable
+{
+    private readonly Dictionary<int, bool> _episodeExpanded = new();
+    private HashSet<AniDbEpisode> _episodes = default!;
+    private HashSet<EpisodeWatchedState> _epWatchedStates = default!;
+
+    [Parameter]
+    [EditorRequired]
+    public int AnimeId { get; set; }
+
+
+    [Inject]
+    public IDbContextFactory<ShizouContext> ContextFactory { get; set; } = default!;
+
+    [Inject]
+    public WatchStateService WatchStateService { get; set; } = default!;
+
+    protected override void OnInitialized()
+    {
+        using var context = ContextFactory.CreateDbContext();
+        var epResult = (from ep in context.AniDbEpisodes
+            where ep.AniDbAnimeId == AnimeId
+            join ws in context.EpisodeWatchedStates on ep.Id equals ws.Id into wslj
+            from ws in wslj.DefaultIfEmpty()
+            select new { ep, ws }).ToList();
+        _episodes = epResult.Select(r => r.ep).ToHashSet();
+        _epWatchedStates = epResult.Select(r => r.ws).ToHashSet();
+        base.OnInitialized();
+    }
+
+
+    private void ToggleEpExpand(AniDbEpisode ep)
+    {
+        if (_episodeExpanded.TryGetValue(ep.Id, out var expanded))
+            _episodeExpanded[ep.Id] = !expanded;
+        else
+            _episodeExpanded[ep.Id] = true;
+    }
+
+    private List<(AniDbFile, FileWatchedState?, LocalFile?)> GetAniDbFiles(int episodeId)
+    {
+        using var context = ContextFactory.CreateDbContext();
+        var result = (from f in context.AniDbFiles.Include(f => f.AniDbGroup)
+            join xref in context.AniDbEpisodeFileXrefs on f.Id equals xref.AniDbFileId
+            where xref.AniDbEpisodeId == episodeId
+            join lf in context.LocalFiles on f.Ed2k equals lf.Ed2k into lflj
+            from lf in lflj.DefaultIfEmpty()
+            join ws in context.FileWatchedStates on f.Id equals ws.Id into wslj
+            from ws in wslj.DefaultIfEmpty()
+            select new { f, ws, lf }).ToList();
+        return result.Select(x => (x.f, x.ws, x.lf)).ToList();
+    }
+
+    private List<LocalFile> GetManualLinks(int episodeId)
+    {
+        using var context = ContextFactory.CreateDbContext();
+        var result = (from lf in context.LocalFiles
+            join xref in context.ManualLinkXrefs on lf.Id equals xref.LocalFileId
+            where xref.AniDbEpisodeId == episodeId
+            select lf).ToList();
+        return result;
+    }
+}
---
+++ Shizou.Blazor/Features/Anime/Components/FileCard.razor.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Components;
+using Shizou.Data;
+using Shizou.Data.Models;
+using Shizou.Server.Services;
+
+namespace Shizou.Blazor.Features.Anime.Components;
+
+public partial class FileCard
+{
+    private int? _videoOpen;
+    private IWatchedState _watchedState = default!;
+
+    [CascadingParameter(Name = "IdentityCookie")]
+    public string? IdentityCookie { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public AniDbFile? AniDbFile { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public FileWatchedState? FileWatchedState { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public LocalFile? LocalFile { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public EpisodeWatchedState EpisodeWatchedState { get; set; } = default!;
+
+    [Inject]
+    public WatchStateService WatchStateService { get; set; } = default!;
+
+
+    [Inject]
+    public NavigationManager NavigationManager { get; set; } = default!;
+
+
+    protected override void OnInitialized()
+    {
+        _watchedState = FileWatchedState as IWatchedState ?? EpisodeWatchedState;
+        base.OnInitialized();
+    }
+
+    private void Mark(bool watched)
+    {
+        switch (_watchedState)
+        {
+            case Data.Models.FileWatchedState:
+                if (WatchStateService.MarkFile(_watchedState.Id, watched))
+                    _watchedState.Watched = watched;
+                break;
+            case Data.Models.EpisodeWatchedState:
+                if (WatchStateService.MarkEpisode(_watchedState.Id, watched))
+                    _watchedState.Watched = watched;
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(_watchedState));
+        }
+    }
+
+    private void MarkUnwatched()
+    {
+    }
+
+    private void OpenVideo(int localFileId)
+    {
+        _videoOpen = localFileId;
+    }
+
+    private void CloseVideo()
+    {
+        _videoOpen = null;
+    }
+
+    private void OpenInMpv(LocalFile file)
+    {
+        if (IdentityCookie is null)
+            return;
+        NavigationManager.NavigateTo(
+            $"mpv:{NavigationManager.BaseUri}api/FileServer/{file.Id}{Path.GetExtension(file.PathTail)}?{Constants.IdentityCookieName}={IdentityCookie}");
+    }
+}
