4afc2445685d7b786a0d8f458fd8ab44728190cf
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-09-18 01:29:34 -0700

    Prepare seeded database class for tests

--- Shizou.Tests/DatabaseTests.cs
+++ Shizou.Tests/DatabaseTests.cs
@@ -1,14 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
 using Shizou.Data.Database;
 
 namespace Shizou.Tests;
 
 [TestClass]
-public class DatabaseTests
+public class DatabaseTests : SeededDatabaseTests
 {
     [TestMethod]
     public void TestDateTimeConversion()
     {
-        using var context = new ShizouContext();
+        var provider = GetServiceCollection().BuildServiceProvider();
+        using var context = provider.GetRequiredService<ShizouContext>();
         var result = context.AniDbAnimes.Where(a => a.Updated < DateTime.UtcNow).ToList();
         Assert.IsNotNull(result);
     }
---
+++ Shizou.Tests/SeededDatabaseTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Shizou.Data;
+using Shizou.Data.Database;
+using Shizou.Data.Enums;
+using Shizou.Data.Models;
+
+namespace Shizou.Tests;
+
+[TestClass]
+public class SeededDatabaseTests
+{
+    private static readonly string ConnectionString = $"Data Source={Path.Combine(FilePaths.ApplicationDataDir, "ShizouTestDB.sqlite3")};Foreign Keys=True;";
+
+    private static readonly object Lock = new();
+    private static bool _databaseInitialized;
+
+    public SeededDatabaseTests()
+    {
+        lock (Lock)
+        {
+            if (!_databaseInitialized)
+            {
+                using var scope = GetServiceCollection().BuildServiceProvider().CreateScope();
+                using var context = scope.ServiceProvider.GetRequiredService<ShizouContext>();
+                context.Database.EnsureDeleted();
+                context.Database.Migrate();
+                context.AniDbAnimes.AddRange(new AniDbAnime
+                {
+                    Id = 1,
+                    Title = "blah",
+                    AnimeType = AnimeType.TvSeries,
+                    EpisodeCount = 12,
+                    AirDate = "2022-02-15",
+                    EndDate = null,
+                    Description = "testdescription",
+                    Restricted = false,
+                    ImageFilename = null,
+                    Updated = default
+                });
+
+                context.SaveChanges();
+
+                _databaseInitialized = true;
+            }
+        }
+    }
+
+    protected static ServiceCollection GetServiceCollection()
+    {
+        var collection = new ServiceCollection();
+        collection.AddDbContextFactory<ShizouContext>(opts => opts.UseSqlite(ConnectionString));
+        collection.AddLogging();
+        return collection;
+    }
+}
