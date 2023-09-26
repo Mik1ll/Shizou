9cdb5ca7a0504b1d00ff4b6e6775623c16c681c3
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-09-26 06:46:16 -0700

    Add select table component

---
+++ Shizou.Blazor/Features/Components/SelectRow.razor.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Shizou.Blazor.Features.Components;
+
+public partial class SelectRow<TValue>
+{
+    private HashSet<string> _classes = default!;
+
+    [CascadingParameter]
+    public SelectTable<TValue> ParentTable { get; set; } = default!;
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object> AdditionalAttributes { get; set; } = new();
+
+    [Parameter]
+    [EditorRequired]
+    public int Index { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public TValue Value { get; set; } = default!;
+
+    public bool Active
+    {
+        get => _classes.Contains("table-active");
+        set
+        {
+            if (value ? _classes.Add("table-active") : _classes.Remove("table-active"))
+                StateHasChanged();
+        }
+    }
+
+    protected override void OnInitialized()
+    {
+        ParentTable.AddChild(this);
+        _classes = new HashSet<string>();
+    }
+
+    protected override void OnParametersSet()
+    {
+        var active = Active;
+        AdditionalAttributes.Remove("class", out var addClasses);
+        if (addClasses is string s)
+            _classes = s.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToHashSet();
+        Active = active;
+    }
+
+    private async Task Clicked(MouseEventArgs args)
+    {
+        await ParentTable.RowClicked(this, args);
+    }
+}
---
+++ Shizou.Blazor/Features/Components/SelectTable.razor.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Shizou.Blazor.Features.Components;
+
+public partial class SelectTable<TValue>
+{
+    private HashSet<SelectRow<TValue>> _rows = default!;
+    private SelectRow<TValue>? _lastClicked;
+
+
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    [EditorRequired]
+    public EventCallback<List<TValue>> OnChange { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public Dictionary<string, object> AdditionalAttributes { get; set; } = new();
+
+    public void AddChild(SelectRow<TValue> row)
+    {
+        _rows.Add(row);
+    }
+
+    public async Task RowClicked(SelectRow<TValue> row, MouseEventArgs args)
+    {
+        if (args.ShiftKey && _lastClicked is not null && row != _lastClicked)
+        {
+            var low = Math.Min(row.Index, _lastClicked.Index);
+            var high = Math.Max(row.Index, _lastClicked.Index);
+            foreach (var x in _rows)
+                x.Active = false;
+            foreach (var r in _rows.Where(r => low <= r.Index && r.Index <= high))
+                r.Active = true;
+        }
+        else if (args.CtrlKey)
+        {
+            row.Active = !row.Active;
+            _lastClicked = row;
+        }
+        else
+        {
+            foreach (var x in _rows)
+                x.Active = false;
+            row.Active = true;
+            _lastClicked = row;
+        }
+
+        await Changed();
+    }
+
+    public async Task Changed()
+    {
+        await OnChange.InvokeAsync(_rows.Where(r => r.Active).Select(r => r.Value).ToList());
+    }
+
+    protected override void OnInitialized()
+    {
+        _rows = new HashSet<SelectRow<TValue>>();
+    }
+}
