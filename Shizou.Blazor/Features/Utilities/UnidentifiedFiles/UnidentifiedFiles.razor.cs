800f1b328e71b3b77b689899f0faa09b61520337
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-09-26 07:09:53 -0700

    Start work on unidentified files utility

---
+++ Shizou.Blazor/Features/Utilities/UnidentifiedFiles/UnidentifiedFiles.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using Shizou.Data.Database;
+using Shizou.Data.Models;
+
+namespace Shizou.Blazor.Features.Utilities.UnidentifiedFiles;
+
+public partial class UnidentifiedFiles
+{
+    private List<LocalFile> _localFiles = default!;
+
+    [Inject]
+    public IDbContextFactory<ShizouContext> ContextFactory { get; set; } = default!;
+
+
+    protected override void OnInitialized()
+    {
+        using var context = ContextFactory.CreateDbContext();
+        _localFiles = (from lf in context.LocalFiles.Include(lf => lf.ImportFolder)
+            where lf.ManualLinkEpisodeId == null && !context.AniDbFiles.Any(f => f.Ed2k == lf.Ed2k)
+            select lf).ToList();
+    }
+
+    private void OnSelectChanged(List<LocalFile> values)
+    {
+        ;
+    }
+}
