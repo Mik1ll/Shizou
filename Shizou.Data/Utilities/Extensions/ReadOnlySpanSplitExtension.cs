2a54ba9f0534464f0dc18d41ce9fbbc666bab36a
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-10-09 00:57:16 -0700

    Implement anime title search

---
+++ Shizou.Data/Utilities/Extensions/ReadOnlySpanSplitExtension.cs
@@ -0,0 +1,51 @@
+﻿namespace Shizou.Data.Utilities.Extensions;
+
+public static class ReadOnlySpanSplitExtension
+{
+    public static SplitEnumerator SplitSpan(this string str, char seperator, params char[] seperators)
+    {
+        return new SplitEnumerator(str, new[] { seperator }.Concat(seperators).ToArray());
+    }
+
+    public static SplitEnumerator SplitSpan(this ReadOnlySpan<char> str, char seperator, params char[] seperators)
+    {
+        return new SplitEnumerator(str, new[] { seperator }.Concat(seperators).ToArray());
+    }
+
+    public ref struct SplitEnumerator
+    {
+        private ReadOnlySpan<char> _str;
+        private readonly ReadOnlySpan<char> _seperators;
+
+        public ReadOnlySpan<char> Current { get; private set; }
+
+        public SplitEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public SplitEnumerator(ReadOnlySpan<char> str, ReadOnlySpan<char> seperators)
+        {
+            _str = str;
+            _seperators = seperators;
+            Current = default;
+        }
+
+        public bool MoveNext()
+        {
+            if (_str.Length == 0)
+                return false;
+            var index = _str.IndexOfAny(_seperators);
+            if (index == -1)
+            {
+                Current = _str;
+                _str = ReadOnlySpan<char>.Empty;
+                return true;
+            }
+
+            Current = _str.Slice(0, index);
+            _str = _str.Slice(index + 1);
+            return true;
+        }
+    }
+}
--- Shizou.Server/Services/AnimeTitleSearchService.cs
+++ Shizou.Server/Services/AnimeTitleSearchService.cs
@@ -1,21 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using FuzzySharp;
+using FuzzySharp.SimilarityRatio;
+using FuzzySharp.SimilarityRatio.Scorer.StrategySensitive;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Shizou.Data;
 using Shizou.Data.Database;
 using Shizou.Data.Enums;
 using Shizou.Data.Models;
+using Shizou.Data.Utilities.Extensions;
 
 namespace Shizou.Server.Services;
 
 public class AnimeTitleSearchService
 {
+    private static readonly Regex RemoveSpecial = new(@"[][`!@#$%^&*()+_=\\.,<>?/;:'""-]", RegexOptions.Compiled);
     private readonly ILogger<AnimeTitleSearchService> _logger;
     private readonly IHttpClientFactory _clientFactory;
     private readonly IDbContextFactory<ShizouContext> _contextFactory;
@@ -30,7 +37,23 @@ public class AnimeTitleSearchService
         _contextFactory = contextFactory;
     }
 
-    public async Task<string?> GetContent()
+    private enum TitleType
+    {
+        Primary = 1,
+        Synonym = 2,
+        Short = 3,
+        Official = 4
+    }
+
+    public async Task<List<(int, string)>?> Search(string query)
+    {
+        var data = await GetContent();
+        if (data is null) return null;
+        var titles = ParseContent(data);
+        return SearchTitles(titles, query).Select(t => (t.Aid, t.Title)).ToList();
+    }
+
+    private async Task<string?> GetContent()
     {
         // ReSharper disable once MethodHasAsyncOverload
         // ReSharper disable once UseAwaitUsing
@@ -76,7 +99,64 @@ public class AnimeTitleSearchService
         return data;
     }
 
-    public void ParseContent(string content)
+    private List<AnimeTitle> ParseContent(string content)
     {
+        var titles = new List<AnimeTitle>();
+        foreach (var line in content.SplitSpan('\n'))
+        {
+            if (line.IsEmpty || line.StartsWith(new ReadOnlySpan<char>('#')))
+                continue;
+            var enumerator = line.SplitSpan('|');
+            enumerator.MoveNext();
+            var aid = int.Parse(enumerator.Current);
+            enumerator.MoveNext();
+            var type = Enum.Parse<TitleType>(enumerator.Current);
+            enumerator.MoveNext();
+            var lang = enumerator.Current;
+            enumerator.MoveNext();
+            var title = enumerator.Current;
+            titles.Add(new AnimeTitle(aid, type, lang.ToString(), title.ToString()));
+        }
+
+        return titles;
     }
+
+    private List<AnimeTitle> SearchTitles(List<AnimeTitle> titles, string query)
+    {
+        string Processor(AnimeTitle title)
+        {
+            return RemoveSpecial.Replace(title.Title.ToLower().Trim(), string.Empty);
+        }
+
+        var results = Process.ExtractTop(new AnimeTitle(0, TitleType.Primary, "", query), titles, Processor, ScorerCache.Get<TokenSetScorer>(), 50).ToList();
+        var refinedResults = results.GroupBy(r => r.Value.Aid)
+            .Select(g => g
+                .OrderBy(r => r.Value.Type switch
+                {
+                    TitleType.Primary => 0,
+                    TitleType.Official => 1,
+                    TitleType.Synonym => 2,
+                    TitleType.Short => 3,
+                    _ => int.MaxValue
+                })
+                .ThenBy(r => r.Value.Lang switch
+                {
+                    "x-jat" => 0,
+                    "x-kot" => 0,
+                    "x-zht" => 0,
+                    "x-other" => 0,
+                    "x-unk" => 0,
+                    "en" => 1,
+                    "ja" => 2,
+                    "ko" => 2,
+                    "zh" => 2,
+                    "zh-Hans" => 2,
+                    _ => int.MaxValue
+                })
+                .ThenByDescending(r => r.Score).First())
+            .OrderByDescending(r => r.Score).Select(r => r.Value).ToList();
+        return refinedResults;
+    }
+
+    private record AnimeTitle(int Aid, TitleType Type, string Lang, string Title);
 }
