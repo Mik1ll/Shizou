9f583cb1c169568302a2ed4ee04f3838f3d3fc2a
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-10-09 22:09:48 -0700

    Add focus trap to modal

---
+++ Shizou.Blazor/Features/Components/ModalWrapper.razor.cs
@@ -0,0 +1,24 @@
+﻿using Blazored.Modal;
+using Microsoft.AspNetCore.Components;
+
+namespace Shizou.Blazor.Features.Components;
+
+public partial class ModalWrapper
+{
+    private FocusTrap? _focusTrap;
+
+    [CascadingParameter]
+    private BlazoredModalInstance ModalInstance { get; set; } = default!;
+
+    [Parameter]
+    public RenderFragment ChildContent { get; set; } = default!;
+
+    [Parameter]
+    [EditorRequired]
+    public EventCallback Cancel { get; set; }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        ModalInstance.FocusTrap = _focusTrap;
+    }
+}
