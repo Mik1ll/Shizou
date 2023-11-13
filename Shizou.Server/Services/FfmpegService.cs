302be83ba29c99092e970b33ea2fd67f0161de63
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-11-13 04:11:41 -0800

    Ffmpeg thumbnail generation fixes
    * Fix entire rest of file being decoded due to select filter
    * Fix duration not being fetched properly, including edge cases with mkv
     file duration being wrong sometimes
     * Ensure video streams are used, capital V excludes attached image
     streams

--- Shizou.Server/Services/FfmpegService.cs
+++ Shizou.Server/Services/FfmpegService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 
@@ -88,17 +90,39 @@ public class FfmpegService
     {
         using var process = NewFfprobeProcess();
         process.StartInfo.Arguments =
-            $"-v fatal -select_streams v:0 -show_entries format=duration:stream=duration -of csv \"{fileInfo.FullName}\"";
+            $"-v fatal -select_streams V -show_entries format=duration:stream=duration:stream_tags -sexagesimal -of json \"{fileInfo.FullName}\"";
         process.Start();
-        var durations = (await process.StandardOutput.ReadToEndAsync()).Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-            .Select(s => s.Split(',')).ToDictionary(s => s[0], s => s[1]);
-        if (durations.TryGetValue("format", out var formatDurStr) && double.TryParse(formatDurStr, out var formatDur))
-            return formatDur;
-        if (durations.TryGetValue("stream", out var streamDurStr) && double.TryParse(streamDurStr, out var streamDur))
-            return streamDur;
+        using var doc = await JsonDocument.ParseAsync(process.StandardOutput.BaseStream);
+        var rootEl = doc.RootElement;
+        if (rootEl.TryGetProperty("streams", out var streamsEl))
+            foreach (var stream in streamsEl.EnumerateArray())
+            {
+                if (stream.TryGetProperty("tags", out var tagsEl))
+                    foreach (var tagDurProp in tagsEl.EnumerateObject().Where(p => p.Name.StartsWith("duration", StringComparison.OrdinalIgnoreCase)))
+                        if (tagDurProp.Value.GetString() is { } durStr)
+                            if (TimeSpan.TryParse(TrimFractional(durStr), CultureInfo.InvariantCulture, out var tagDur))
+                                return tagDur.TotalSeconds;
+
+                if (rootEl.TryGetProperty("duration", out var streamDurEl))
+                    if (streamDurEl.GetString() is { } streamDurStr)
+                        if (TimeSpan.TryParse(TrimFractional(streamDurStr), CultureInfo.InvariantCulture, out var streamDur))
+                            return streamDur.TotalSeconds;
+            }
+
+        if (rootEl.TryGetProperty("format", out var formatEl))
+            if (formatEl.TryGetProperty("duration", out var formatDurEl))
+                if (formatDurEl.GetString() is { } formatDurStr)
+                    if (TimeSpan.TryParse(TrimFractional(formatDurStr), CultureInfo.InvariantCulture, out var formatDur))
+                        return formatDur.TotalSeconds;
 
         _logger.LogWarning("Could not get a duration for file \"{FilePath}\"", fileInfo.FullName);
         return null;
+
+        // Trim fractional value https://learn.microsoft.com/en-us/dotnet/api/system.timespan.parse?view=net-7.0#notes-to-callers
+        string TrimFractional(string durStr)
+        {
+            return durStr.LastIndexOf('.') is var idx && idx >= 0 && durStr.Length - 1 >= idx + 8 ? durStr.Remove(idx + 8) : durStr;
+        }
     }
 
     public async Task ExtractThumbnail(FileInfo fileInfo, double duration, string outputPath)
@@ -106,8 +130,13 @@ public class FfmpegService
         if (Path.GetDirectoryName(outputPath) is { Length: > 0 } parentPath)
             Directory.CreateDirectory(parentPath);
         using var process = NewFfmpegProcess();
+        var fps = 3;
+        var thumbnailWindow = 30;
+        var height = 480;
         process.StartInfo.Arguments =
-            $"-v fatal -ss {duration * .4} -i \"{fileInfo.FullName}\" -map 0:v:0 -vf \"fps=3,select='min(eq(selected_n,0)+gt(scene,0.5),1)',thumbnail=90,scale=-2:480\" -frames:v 1 -pix_fmt yuv420p -c:v libwebp -compression_level 6 -preset drawing \"{outputPath}\"";
+            $"-v fatal -y -ss {duration * .4} -t {thumbnailWindow} -i \"{fileInfo.FullName}\" -map 0:V:0 " +
+            $"-vf \"fps={fps},select='min(eq(selected_n,0)+gt(scene,0.4),1)',thumbnail={thumbnailWindow * fps},scale=-2:{height}\" " +
+            $"-frames:v 1 -pix_fmt yuv420p -c:v libwebp -compression_level 6 -preset drawing \"{outputPath}\"";
         process.Start();
         await process.WaitForExitAsync();
     }
@@ -115,7 +144,7 @@ public class FfmpegService
     public async Task<bool> HasVideo(FileInfo fileInfo)
     {
         using var process = NewFfprobeProcess();
-        process.StartInfo.Arguments = $"-v fatal -select_streams v -show_entries stream=codec_name -of csv \"{fileInfo.FullName}\"";
+        process.StartInfo.Arguments = $"-v fatal -select_streams V -show_entries stream=codec_name -of csv \"{fileInfo.FullName}\"";
         process.Start();
         var hasVideo = await process.StandardOutput.ReadToEndAsync();
         return !string.IsNullOrWhiteSpace(hasVideo);
