0a44d07edec7824daa481279f526318dcf30ebc8
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-11-19 20:01:17 -0800

    Create dbcontext and dbset interface without async

---
+++ Shizou.Data/Database/IShizouDbSet.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Shizou.Data.Database;
+
+public interface IShizouDbSet<TEntity> where TEntity : class
+{
+    bool Equals(object? obj);
+    int GetHashCode();
+    string? ToString();
+    IQueryable<TEntity> AsQueryable();
+    TEntity? Find(params object?[]? keyValues);
+    EntityEntry<TEntity> Add(TEntity entity);
+    EntityEntry<TEntity> Attach(TEntity entity);
+    EntityEntry<TEntity> Remove(TEntity entity);
+    EntityEntry<TEntity> Update(TEntity entity);
+    void AddRange(params TEntity[] entities);
+    void AddRange(IEnumerable<TEntity> entities);
+    void AttachRange(params TEntity[] entities);
+    void AttachRange(IEnumerable<TEntity> entities);
+    void RemoveRange(params TEntity[] entities);
+    void RemoveRange(IEnumerable<TEntity> entities);
+    void UpdateRange(params TEntity[] entities);
+    void UpdateRange(IEnumerable<TEntity> entities);
+    EntityEntry<TEntity> Entry(TEntity entity);
+    IEntityType EntityType { get; }
+    LocalView<TEntity> Local { get; }
+}
---
+++ Shizou.Data/Database/ShizouDbSet.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Shizou.Data.Database;
+
+public class ShizouDbSet<TEntity> : IShizouDbSet<TEntity>
+    where TEntity : class
+{
+    private readonly DbSet<TEntity> _dbSet;
+
+    public ShizouDbSet(DbSet<TEntity> dbSet)
+    {
+        _dbSet = dbSet;
+    }
+
+    public IQueryable<TEntity> AsQueryable()
+    {
+        return _dbSet.AsQueryable();
+    }
+
+    public TEntity? Find(params object?[]? keyValues)
+    {
+        return _dbSet.Find(keyValues);
+    }
+
+    public EntityEntry<TEntity> Add(TEntity entity)
+    {
+        return _dbSet.Add(entity);
+    }
+
+    public EntityEntry<TEntity> Attach(TEntity entity)
+    {
+        return _dbSet.Attach(entity);
+    }
+
+    public EntityEntry<TEntity> Remove(TEntity entity)
+    {
+        return _dbSet.Remove(entity);
+    }
+
+    public EntityEntry<TEntity> Update(TEntity entity)
+    {
+        return _dbSet.Update(entity);
+    }
+
+    public void AddRange(params TEntity[] entities)
+    {
+        _dbSet.AddRange(entities);
+    }
+
+    public void AddRange(IEnumerable<TEntity> entities)
+    {
+        _dbSet.AddRange(entities);
+    }
+
+    public void AttachRange(params TEntity[] entities)
+    {
+        _dbSet.AttachRange(entities);
+    }
+
+    public void AttachRange(IEnumerable<TEntity> entities)
+    {
+        _dbSet.AttachRange(entities);
+    }
+
+    public void RemoveRange(params TEntity[] entities)
+    {
+        _dbSet.RemoveRange(entities);
+    }
+
+    public void RemoveRange(IEnumerable<TEntity> entities)
+    {
+        _dbSet.RemoveRange(entities);
+    }
+
+    public void UpdateRange(params TEntity[] entities)
+    {
+        _dbSet.UpdateRange(entities);
+    }
+
+    public void UpdateRange(IEnumerable<TEntity> entities)
+    {
+        _dbSet.UpdateRange(entities);
+    }
+
+    public EntityEntry<TEntity> Entry(TEntity entity)
+    {
+        return _dbSet.Entry(entity);
+    }
+
+    public IEntityType EntityType => _dbSet.EntityType;
+    public LocalView<TEntity> Local => _dbSet.Local;
+}
