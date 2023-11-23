55eec4579acd429d887beca148d3b96e63c8fcd0
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-11-23 02:53:03 -0800

    Add PolymorphicJsonTypeResolver.cs

---
+++ Shizou.Data/Utilities/PolymorphicJsonTypeResolver.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace Shizou.Data.Utilities;
+
+public class PolymorphicJsonTypeResolver<T> : DefaultJsonTypeInfoResolver
+{
+    public override JsonTypeInfo GetTypeInfo(Type type, JsonSerializerOptions options)
+    {
+        var typeInfo = base.GetTypeInfo(type, options);
+        if (typeInfo.Type == typeof(T))
+            return PolymorphicJsonTypeInfo<T>.CreateJsonTypeInfo(options);
+        return typeInfo;
+    }
+}
