091db3e32a02e9e21fefadfe1d3d0b6e47cf916e
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-12-11 06:03:50 -0800

    Add linux scheme handler installer

--- Shizou.SchemeHandler/Program.cs
+++ Shizou.SchemeHandler/Program.cs
@@ -20,11 +20,38 @@ switch (args[0])
             using var command = key.CreateSubKey(@"shell\open\command");
             command.SetValue("", $"\"{location}\" \"%1\"");
         }
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            var desktopContent =
+                "[Desktop Entry]\n" +
+                "Type=Application\n" +
+                "Name=Shizou External Player\n" +
+                $"TryExec={location}\n" +
+                $"Exec={location} %u\n" +
+                "Terminal=false\n" +
+                "StartupNotify=false\n" +
+                "MimeType=x-scheme-handler/shizou;\n";
+            var desktopDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local/share/applications");
+            var desktopName = "shizou.desktop";
+            var desktopPath = Path.Combine(desktopDir, desktopName);
+            Directory.CreateDirectory(desktopDir);
+            File.WriteAllText(desktopPath, desktopContent);
+            Process.Start("desktop-file-install", $"-m 755 --dir={desktopDir.Replace(" ", @"\ ")} --rebuild-mime-info-cache {desktopName}").WaitForExit();
+        }
 
         break;
     case "--uninstall":
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             Registry.CurrentUser.DeleteSubKeyTree(@"SOFTWARE\Classes\shizou");
+        else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            var desktopDir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".local/share/applications");
+            var desktopName = "shizou.desktop";
+            var desktopPath = Path.Combine(desktopDir, desktopName);
+            File.Delete(desktopPath);
+            Process.Start("update-desktop-database", desktopDir.Replace(" ", @"\ "));
+        }
+
         break;
     default:
         var uri = new Uri(args[0]);
