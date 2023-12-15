bfbe120ea46e4cc572d51f15d7230638ccb0d8e8
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-12-15 02:59:14 -0800

    Move rhash replacement into docker file

--- Scripts/docker-build-rhash.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-apk update
-apk add gcc musl-dev
-
-cd /app/RHash
-wget -qO- https://github.com/rhash/RHash/archive/refs/tags/v1.4.4.tar.gz | tar -xz
-cd RHash-1.4.4
-./configure && make
-cp librhash/librhash.so.1.4.4 ../librhash.so
