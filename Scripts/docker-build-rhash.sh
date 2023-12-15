a8bd346bb1e3e870d5b47a3194f06e2659bc5451
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-12-14 18:32:05 -0800

    Move setpw to seperate file

--- Scripts/docker-build-rhash.sh
+++ Scripts/docker-build-rhash.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
-wget --no-check-certificate '--post-data="password"' '--header=Content-Type: application/json' 'https://localhost/api/Account/SetPassword'
 apk update
 apk add gcc musl-dev
-
 
 cd /app/RHash
 wget -qO- https://github.com/rhash/RHash/archive/refs/tags/v1.4.4.tar.gz | tar -xz
---
+++ Scripts/docker-setpw.sh
@@ -0,0 +1,2 @@
+﻿#! /bin/sh
+wget --no-check-certificate '--post-data="password"' '--header=Content-Type: application/json' 'https://localhost/api/Account/SetPassword'
