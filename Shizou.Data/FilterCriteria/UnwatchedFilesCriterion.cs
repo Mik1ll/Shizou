c05ab7cf437e06f1f5cdd543ab1fab86225d5645
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2023-12-30 14:52:11 -0800

    Remove credits and trailers in unwatched criterion

--- Shizou.Data/FilterCriteria/UnwatchedFilesCriterion.cs
+++ Shizou.Data/FilterCriteria/UnwatchedFilesCriterion.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq.Expressions;
+using Shizou.Data.Enums;
 using Shizou.Data.Models;
 
 namespace Shizou.Data.FilterCriteria;
@@ -8,6 +9,7 @@ public record UnwatchedFilesCriterion(bool Negated) : TermCriterion(Negated)
     protected override Expression<Func<AniDbAnime, bool>> MakeTerm()
     {
         return anime => anime.AniDbEpisodes.Any(e =>
-            e.AniDbFiles.Any(f => f.FileWatchedState.Watched == false) || (e.ManualLinkLocalFiles.Any() && e.EpisodeWatchedState.Watched == false));
+            e.EpisodeType != EpisodeType.Credits && e.EpisodeType != EpisodeType.Trailer &&
+            (e.AniDbFiles.Any(f => f.FileWatchedState.Watched == false) || (e.ManualLinkLocalFiles.Any() && e.EpisodeWatchedState.Watched == false)));
     }
 }
