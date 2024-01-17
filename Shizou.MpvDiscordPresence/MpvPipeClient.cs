61f5220b58112de5572350efed47c4f6bf2b4259
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-01-17 04:40:42 -0800

    Start work on mpv discord presence

---
+++ Shizou.MpvDiscordPresence/MpvPipeClient.cs
@@ -0,0 +1,88 @@
+﻿using System.IO.Pipes;
+using System.Security.Principal;
+using System.Text;
+using System.Text.Json;
+
+namespace Shizou.MpvDiscordPresence;
+
+public class MpvPipeClient : IDisposable
+{
+    private int _nextRequestId;
+
+    // ReSharper disable InconsistentNaming
+    private record Request(string[] command, int request_id);
+
+    private record Response(string error, JsonElement data, int request_id);
+    // ReSharper restore InconsistantNaming
+
+    private readonly NamedPipeClientStream _pipeClientStream;
+
+    public MpvPipeClient(string serverPath)
+    {
+        _pipeClientStream = new NamedPipeClientStream(".", serverPath, PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Anonymous);
+        _pipeClientStream.ReadTimeout = 500;
+        _pipeClientStream.WriteTimeout = 500;
+    }
+
+    public void Connect()
+    {
+        if (!_pipeClientStream.IsConnected)
+            _pipeClientStream.Connect(TimeSpan.FromMilliseconds(500));
+    }
+
+    private Request NewRequest(params string[] command)
+    {
+        _nextRequestId++;
+        return new Request(command, _nextRequestId);
+    }
+
+    public string GetPropertyString(string key)
+    {
+        var request = NewRequest("get_property_string", key);
+        SendRequest(request);
+        var (response, error) = ReceiveResponse(request);
+        if (error is not null)
+            return "";
+        return response?.GetString() ?? "";
+    }
+
+    private (JsonElement? data, string? error) ReceiveResponse(Request request)
+    {
+        try
+        {
+            var buffer = new byte[1024];
+            var bytesRead = 0;
+
+            using var ms = new MemoryStream();
+            do
+            {
+                bytesRead += _pipeClientStream.Read(buffer, 0, buffer.Length);
+                ms.Write(buffer, 0, bytesRead);
+            } while (bytesRead != 0 && buffer[bytesRead - 1] != '\n');
+
+            var response = JsonSerializer.Deserialize<Response>(ms);
+            if (response?.request_id != request.request_id)
+                return (null, "Request ID does not match");
+            if (response.error != "success")
+                return (null, "Response returned error");
+            return (response.data, null);
+        }
+        catch (Exception ex)
+        {
+            return (null, $"Read threw exception: {ex}");
+        }
+    }
+
+    private void SendRequest(Request request)
+    {
+        Connect();
+        var requestJson = JsonSerializer.Serialize(request) + '\n';
+        var requestBytes = Encoding.UTF8.GetBytes(requestJson);
+        _pipeClientStream.Write(requestBytes);
+    }
+
+    public void Dispose()
+    {
+        _pipeClientStream.Dispose();
+    }
+}
