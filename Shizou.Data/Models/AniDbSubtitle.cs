8ea9250fc5a1d1e4fd9c15599d0e90187507b8e1
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-01-28 19:17:25 -0800

    Remove unneeded Owned attribute

--- Shizou.Data/Models/AniDbSubtitle.cs
+++ Shizou.Data/Models/AniDbSubtitle.cs
@@ -1,8 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿namespace Shizou.Data.Models;
 
-namespace Shizou.Data.Models;
-
-[Owned]
 public class AniDbSubtitle
 {
     public required string Language { get; set; }
