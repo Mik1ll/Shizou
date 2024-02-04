061f1d48a933b994e6054fe95694ac1413648f32
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-02-04 00:48:19 -0800

    Update settings json schema

--- Shizou.Server/Options/ShizouOptions.cs
+++ Shizou.Server/Options/ShizouOptions.cs
@@ -5,137 +5,119 @@ using System.Text.Json.Serialization;
 using Shizou.Data;
 using Shizou.Data.Enums;
 
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+
 namespace Shizou.Server.Options;
 
 public class ShizouOptions
 {
     public const string Shizou = "Shizou";
 
-    public const string Schema = /*lang=json*/ """
-                                               {
-                                                 "$schema": "https://json-schema.org/draft-07/schema",
-                                                 "type": "object",
-                                                 "properties": {
-                                                   "Shizou": {
-                                                     "type": "object",
-                                                     "properties": {
-                                                       "Import": {
-                                                         "type": "object"
-                                                       },
-                                                       "AniDb": {
-                                                         "type": "object",
-                                                         "description": "Change AniDb config here",
-                                                         "properties": {
-                                                           "Username": {
-                                                             "description": "AniDb Username",
-                                                             "type": "string"
-                                                           },
-                                                           "Password": {
-                                                             "description": "Ensure AniDb password is not use anywhere else, it is insecure. Only use alphanumeric characters.",
-                                                             "type": "string",
-                                                             "pattern": "^[a-zA-z0-9]+$",
-                                                             "minLength": 4,
-                                                             "maxLength": 64
-                                                           },
-                                                           "ServerHost": {
-                                                             "description": "Host name of AniDb API (fully qualified domain name)",
-                                                             "type": "string",
-                                                             "format": "hostname"
-                                                           },
-                                                           "ImageServerHost": {
-                                                             "description": "Host name of AniDb image server, this is autopopulated",
-                                                             "type": [
-                                                               "string",
-                                                               "null"
-                                                             ],
-                                                             "format": "hostname"
-                                                           },
-                                                           "UdpServerPort": {
-                                                             "description": "Remote port used to connect to AniDb UDP API, should be 9000",
-                                                             "type": "integer"
-                                                           },
-                                                           "HttpServerPort": {
-                                                             "description": "Remote port used to connect to AniDb HTTP API, should be 9001",
-                                                             "type": "integer"
-                                                           },
-                                                           "ClientPort": {
-                                                             "description": "Local port used to connect to UDP API, NAT may affect the port that AniDb sees",
-                                                             "type": "integer",
-                                                             "minimum": 1024,
-                                                             "maximum": 65535
-                                                           },
-                                                           "MyList": {
-                                                             "type": "object",
-                                                             "properties": {
-                                                               "DisableSync": {
-                                                                 "description": "Disables Sync to AniDb command, use if concerned of AniDb data loss/clobbering",
-                                                                 "type": "boolean"
-                                                               },
-                                                               "AbsentFileState": {
-                                                                 "description": "State to mark files if they are not present in local collection",
-                                                                 "enum": [
-                                                                   "Unknown",
-                                                                   "Internal",
-                                                                   "External",
-                                                                   "Deleted",
-                                                                   "Remote"
-                                                                 ]
-                                                               },
-                                                               "PresentFileState": {
-                                                                 "description": "State to mark files if they are present in local collection",
-                                                                 "enum": [
-                                                                   "Unknown",
-                                                                   "Internal",
-                                                                   "External",
-                                                                   "Deleted",
-                                                                   "Remote"
-                                                                 ]
-                                                               }
-                                                             }
-                                                           },
-                                                           "FetchRelationDepth": {
-                                                             "description": "How many relation layers deep to fetch anime data, this may result in many HTTP requests if each anime has many relations",
-                                                             "type": "integer",
-                                                             "minimum": 0,
-                                                             "maximum": 4
-                                                           }
-                                                         }
-                                                       },
-                                                       "MyAnimeList": {
-                                                         "type": "object",
-                                                         "properties": {
-                                                           "ClientId": {
-                                                             "type": "string"
-                                                           },
-                                                           "MyAnimeListToken": {
-                                                             "type": [
-                                                               "object",
-                                                               "null"
-                                                             ],
-                                                             "properties": {
-                                                               "AccessToken": {
-                                                                 "type": "string"
-                                                               },
-                                                               "AccessExpiration": {
-                                                                 "type": "string",
-                                                                 "format": "date-time"
-                                                               },
-                                                               "RefreshToken": {
-                                                                 "type": "string"
-                                                               },
-                                                               "RefreshExpiration": {
-                                                                 "type": "string",
-                                                                 "format": "date-time"
-                                                               }
-                                                             }
-                                                           }
-                                                         }
-                                                       }
-                                                     }
-                                                   }
-                                                 }
-                                               }
-                                               """;
+    // @formatter:off
+    public const string Schema = /*lang=json*/ $$"""
+{
+    "$schema": "https://json-schema.org/draft-07/schema",
+    "type": "object",
+    "properties": {
+        "{{Shizou}}": {
+            "type": "object",
+            "properties": {
+                "Import": {
+                    "type": "object"
+                },
+                "{{nameof(AniDb)}}": {
+                    "type": "object",
+                    "description": "Change AniDb config here",
+                    "properties": {
+                        "{{nameof(AniDb.Username)}}": {
+                            "description": "AniDb Username",
+                            "type": "string"
+                        },
+                        "{{nameof(AniDb.Password)}}": {
+                            "description": "Ensure AniDb password is not use anywhere else, it is insecure. Only use alphanumeric characters.",
+                            "type": "string",
+                            "pattern": "^[a-zA-z0-9]+$",
+                            "minLength": 4,
+                            "maxLength": 64
+                        },
+                        "{{nameof(AniDb.ServerHost)}}": {
+                            "description": "Host name of AniDb API (fully qualified domain name)",
+                            "type": "string",
+                            "format": "hostname"
+                        },
+                        "{{nameof(AniDb.ImageServerHost)}}": {
+                            "description": "Host name of AniDb image server, this is autopopulated",
+                            "type": [
+                                "string",
+                                "null"
+                            ],
+                            "format": "hostname"
+                        },
+                        "{{nameof(AniDb.UdpServerPort)}}": {
+                            "description": "Remote port used to connect to AniDb UDP API, should be 9000",
+                            "type": "integer"
+                        },
+                        "{{nameof(AniDb.HttpServerPort)}}": {
+                            "description": "Remote port used to connect to AniDb HTTP API, should be 9001",
+                            "type": "integer"
+                        },
+                        "{{nameof(AniDb.ClientPort)}}": {
+                            "description": "Local port used to connect to UDP API, NAT may affect the port that AniDb sees",
+                            "type": "integer",
+                            "minimum": 1024,
+                            "maximum": 65535
+                        },
+                        "{{nameof(AniDb.MyList)}}": {
+                            "type": "object",
+                            "properties": {
+                                "DisableSync": {
+                                    "description": "Disables Sync to AniDb command, use if concerned of AniDb data loss/clobbering",
+                                    "type": "boolean"
+                                },
+                                "{{nameof(AniDb.MyList.AbsentFileState)}}": {
+                                    "description": "State to mark files if they are not present in local collection",
+                                    "enum": [
+                                        "Unknown",
+                                        "Internal",
+                                        "External",
+                                        "Deleted",
+                                        "Remote"
+                                    ]
+                                },
+                                "{{nameof(AniDb.MyList.PresentFileState)}}": {
+                                    "description": "State to mark files if they are present in local collection",
+                                    "enum": [
+                                        "Unknown",
+                                        "Internal",
+                                        "External",
+                                        "Deleted",
+                                        "Remote"
+                                    ]
+                                }
+                            }
+                        },
+                        "{{nameof(AniDb.FetchRelationDepth)}}": {
+                            "description": "How many relation layers deep to fetch anime data, this may result in many HTTP requests if each anime has many relations",
+                            "type": "integer",
+                            "minimum": 0,
+                            "maximum": 4
+                        }
+                    }
+                },
+                "{{nameof(MyAnimeList)}}": {
+                    "type": "object",
+                    "properties": {
+                        "ClientId": {
+                            "type": "string"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+""";
+    // @formatter:on
 
     public ImportOptions Import { get; set; } = new();
 
