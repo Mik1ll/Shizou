17aad185c8aaad32d546a3384bb4d8165afbdc46
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-02-11 04:02:09 -0800

    Don't need select table on missing files util

--- Shizou.Blazor/Features/Utilities/MissingFiles/MissingFIles.razor.cs
+++ Shizou.Blazor/Features/Utilities/MissingFiles/MissingFIles.razor.cs
@@ -2,43 +2,34 @@
 using Microsoft.EntityFrameworkCore;
 using Shizou.Data.Database;
 using Shizou.Data.Models;
-using Shizou.Server.Extensions.Query;
+using Shizou.Server.Services;
 
 namespace Shizou.Blazor.Features.Utilities.MissingFiles;
 
 public partial class MissingFiles
 {
     private List<LocalFile> _localFiles = default!;
-    private List<LocalFile> _selectedFiles = new();
 
     [Inject]
     private IShizouContextFactory ContextFactory { get; set; } = default!;
+
+    [Inject]
+    private ImportService ImportService { get; set; } = default!;
 
     protected override void OnInitialized()
     {
         LoadFiles();
     }
 
-
-    private void OnSelectChanged(List<LocalFile> values)
-    {
-        _selectedFiles = values;
-    }
-
-
     private void LoadFiles()
     {
-        _selectedFiles = [];
         using var context = ContextFactory.CreateDbContext();
-        _localFiles = context.LocalFiles.Include(lf => lf.ImportFolder).Unidentified()
-            .Where(lf => lf.ImportFolder != null).AsEnumerable()
-            .Where(lf => !File.Exists(Path.Combine(lf.ImportFolder!.Path, lf.PathTail))).ToList();
+        _localFiles = context.LocalFiles.Include(lf => lf.ImportFolder).AsEnumerable()
+            .Where(lf => lf.ImportFolder == null || !File.Exists(Path.Combine(lf.ImportFolder.Path, lf.PathTail))).ToList();
     }
 
-
-    private void RemoveMissing(List<LocalFile> files)
+    private void RemoveMissing()
     {
-        // TODO
-        throw new NotImplementedException();
+        ImportService.RemoveMissingFiles();
     }
 }
