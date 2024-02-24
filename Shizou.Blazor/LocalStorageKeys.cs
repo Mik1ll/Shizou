5547408ea2df23027c8538c799d7b31c01161fc2
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-02-23 21:15:46 -0800

    Don't need default value for theme local storage

--- Shizou.Blazor/LocalStorageKeys.cs
+++ Shizou.Blazor/LocalStorageKeys.cs
@@ -5,5 +5,4 @@ public static class LocalStorageKeys
     public static readonly string ExternalPlayerScheme = "shizou.extPlayerScheme";
     public static readonly string ExternalPlayerSchemeDefault = "shizou";
     public static readonly string Theme = "shizou.theme";
-    public static readonly string ThemeDefault = "light";
 }
