763c053360f0cde61509916bafa406840723cc6f
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-03-04 19:08:37 -0800

    Use javascript for theme again
    Fix broken login page theme

--- Shizou.Blazor/Components/Layout/NavMenu.razor.cs
+++ Shizou.Blazor/Components/Layout/NavMenu.razor.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Microsoft.JSInterop;
 
 namespace Shizou.Blazor.Components.Layout;
@@ -7,28 +6,18 @@ namespace Shizou.Blazor.Components.Layout;
 public partial class NavMenu
 {
     private bool _collapsed = true;
-    private bool _isDarkMode = false;
-    private string? _themeColor;
     private bool _expandFileUtils = false;
+    private string _theme = "auto";
+    private Dictionary<string, string> _themeIconClasses = new() { { "light", "bi-sun-fill" }, { "dark", "bi-moon-stars-fill" }, { "auto", "bi-circle-half" } };
 
     private string? NavMenuCssClass => _collapsed ? null : "show";
 
     [Inject]
     private IJSRuntime JsRuntime { get; set; } = default!;
 
-    [Inject]
-    private ProtectedLocalStorage LocalStorage { get; set; } = default!;
-
     protected override async Task OnInitializedAsync()
     {
-        if (await LocalStorage.GetAsync<string>(LocalStorageKeys.Theme) is { Success: true, Value: var themeRes })
-            _isDarkMode = themeRes == "dark";
-        else
-            _isDarkMode = await JsRuntime.InvokeAsync<string>("getPreferredTheme") == "dark";
-
-        if (_isDarkMode)
-            await JsRuntime.InvokeVoidAsync("document.documentElement.setAttribute", "data-bs-theme", "dark");
-        _themeColor = await JsRuntime.InvokeAsync<string>("getThemeColor");
+        _theme = await JsRuntime.InvokeAsync<string>("getStoredTheme");
     }
 
     private void ToggleCollapse() => _collapsed = !_collapsed;
@@ -39,10 +28,6 @@ public partial class NavMenu
 
     private async Task ToggleDarkModeAsync()
     {
-        _isDarkMode = !_isDarkMode;
-        var theme = _isDarkMode ? "dark" : "light";
-        await JsRuntime.InvokeVoidAsync("document.documentElement.setAttribute", "data-bs-theme", theme);
-        _themeColor = await JsRuntime.InvokeAsync<string>("getThemeColor");
-        await LocalStorage.SetAsync(LocalStorageKeys.Theme, theme);
+        _theme = await JsRuntime.InvokeAsync<string>("cycleTheme");
     }
 }
--- Shizou.Blazor/wwwroot/js/theme.js
+++ Shizou.Blazor/wwwroot/js/theme.js
@@ -1,7 +1,27 @@
-﻿function getPreferredTheme() {
-    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+﻿'use strict';
+function getStoredTheme() { return localStorage.getItem('shizou.theme') || 'auto'; }
+function setStoredTheme(theme) { return localStorage.setItem('shizou.theme', theme); }
+function setTheme(theme) {
+    setStoredTheme(theme);
+    if (theme == 'auto') {
+        document.documentElement.setAttribute('data-bs-theme', window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+    } else {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+    }
+    let meta = document.querySelector('meta[name="theme-color"]');
+    if (meta == null) {
+        meta = document.createElement('meta');
+        meta.setAttribute('name', 'theme-color');
+        document.getElementsByTagName('head')[0].appendChild(meta);
+    }
+    meta.setAttribute('content', window.getComputedStyle(document.documentElement).getPropertyValue('--bs-secondary-bg'));
 }
-
-function getThemeColor() {
-    return window.getComputedStyle(document.documentElement).getPropertyValue('--bs-secondary-bg');
+function cycleTheme() {
+    const themes = ['auto', 'light', 'dark'];
+    let currThemeIdx = themes.indexOf(getStoredTheme());
+    if (currThemeIdx < 0) currThemeIdx = 0;
+    const themeIdx = ++currThemeIdx % themes.length;
+    setTheme(themes[themeIdx]);
+    return themes[themeIdx];
 }
+window.onload = e => setTheme(getStoredTheme());
