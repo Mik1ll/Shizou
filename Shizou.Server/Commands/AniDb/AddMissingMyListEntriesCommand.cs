511b439bdc1e48965142c0bb0136dad1189bbe14
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-03-19 18:54:21 -0700

    Update Add missing mylist entries command

--- Shizou.Server/Commands/AniDb/AddMissingMyListEntriesCommand.cs
+++ Shizou.Server/Commands/AniDb/AddMissingMyListEntriesCommand.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -28,36 +28,21 @@ public class AddMissingMyListEntriesCommand : Command<AddMissingMyListEntriesArg
         _options = options.Value;
     }
 
-    protected override Task ProcessInnerAsync() => throw new NotImplementedException();
-    // var filesMissingMyListId = (from ws in _context.FileWatchedStates
-    //     where ws.MyListId == null && ws.AniDbFile.LocalFile != null
-    //     select new { Fid = ws.AniDbFileId, ws.Watched, ws.WatchedUpdated }).ToList();
-    //
-    // var genericFilesMissingMyListId = (from ws in _context.EpisodeWatchedStates
-    //     where ws.MyListId == null && ws.AniDbFileId != null && ws.AniDbEpisode.ManualLinkLocalFiles.Any()
-    //     select new { Fid = ws.AniDbFileId!.Value, ws.Watched, ws.WatchedUpdated }).ToList();
-    //
-    // var episodesWithMissingGenericFile = (from ep in _context.AniDbEpisodes
-    //     where ep.ManualLinkLocalFiles.Any() && ep.EpisodeWatchedState.AniDbFileId == null
-    //     select new { ep.AniDbAnimeId, ep.EpisodeType, ep.Number, ep.EpisodeWatchedState.Watched, ep.EpisodeWatchedState.WatchedUpdated }).ToList();
-    // if (filesMissingMyListId.Count > 0 || genericFilesMissingMyListId.Count > 0)
-    //     _logger.LogInformation(
-    //         "Found {NumFilesMissingMyListId} regular files and {NumGenericFilesMissingMyListId} generic files with missing mylist entries, queueing mylist updates",
-    //         filesMissingMyListId.Count, genericFilesMissingMyListId.Count);
-    // else
-    //     _logger.LogInformation("No files are missing mylist entries");
-    // _commandService.DispatchRange(filesMissingMyListId.Union(genericFilesMissingMyListId).Select(f =>
-    //     new UpdateMyListArgs(false, _options.AniDb.MyList.PresentFileState, f.Watched, f.WatchedUpdated, Fid: f.Fid)));
-    //
-    // if (episodesWithMissingGenericFile.Count > 0)
-    //     _logger.LogInformation("Found {NumEpisodesMissingGenericFile} manually linked episodes with missing generic file, queueing mylist updates",
-    //         episodesWithMissingGenericFile.Count);
-    // else
-    //     _logger.LogInformation("No manually linked episodes are missing generic files");
-    // _commandService.DispatchRange(episodesWithMissingGenericFile.Select(e =>
-    //     new UpdateMyListArgs(false, _options.AniDb.MyList.PresentFileState, e.Watched, e.WatchedUpdated,
-    //         Aid: e.AniDbAnimeId, EpNo: e.EpisodeType.GetEpString(e.Number))));
-    //
-    // Completed = true;
-    // return Task.CompletedTask;
+    protected override Task ProcessInnerAsync()
+    {
+        var filesMissingMyListId = (from ws in _context.FileWatchedStates
+            where ws.MyListId == null && ws.AniDbFile.LocalFiles.Any()
+            select new { Fid = ws.AniDbFileId, ws.Watched, ws.WatchedUpdated }).ToList();
+
+        if (filesMissingMyListId.Count > 0)
+            _logger.LogInformation("Found {NumFilesMissingMyListId} regular files with missing mylist entries, queueing mylist updates",
+                filesMissingMyListId.Count);
+        else
+            _logger.LogInformation("No files are missing mylist entries");
+        _commandService.DispatchRange(filesMissingMyListId.Select(f =>
+            new UpdateMyListArgs(false, _options.AniDb.MyList.PresentFileState, f.Watched, f.WatchedUpdated, Fid: f.Fid)));
+
+        Completed = true;
+        return Task.CompletedTask;
+    }
 }
