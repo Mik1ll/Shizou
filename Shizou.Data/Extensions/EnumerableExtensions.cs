a47d24e5b0864c694d30d68ea8e1871e3e34c167
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-04-05 01:18:09 -0700

    Add alphanumeric sort and use in un-id'd file util

---
+++ Shizou.Data/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Shizou.Data.Extensions;
+
+public static class EnumerableExtensions
+{
+    private static readonly Regex DigitRegex = new(@"\d+", RegexOptions.Compiled | RegexOptions.CultureInvariant);
+
+    public static IOrderedEnumerable<T> OrderByAlphaNumeric<T>(this IEnumerable<T> source, Func<T, string> selector)
+    {
+        var list = source.ToList();
+        var max = list
+            .SelectMany(i => DigitRegex.Matches(selector(i)).Select(m => m.Value.Length))
+            .DefaultIfEmpty().Max();
+
+        return list.OrderBy(i => DigitRegex.Replace(selector(i), m => m.Value.PadLeft(max, '0')));
+    }
+}
