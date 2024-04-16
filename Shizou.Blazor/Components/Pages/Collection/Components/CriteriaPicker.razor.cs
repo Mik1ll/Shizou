f33cecf878b81db3422f08320777490fae861550
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-04-16 15:28:01 -0700

    Add generic files criterion
    Closes #91

--- Shizou.Blazor/Components/Pages/Collection/Components/CriteriaPicker.razor.cs
+++ Shizou.Blazor/Components/Pages/Collection/Components/CriteriaPicker.razor.cs
@@ -12,7 +12,8 @@ public partial class CriteriaPicker
         { nameof(AirDateCriterion), typeof(AirDateCriterion) },
         { nameof(UnwatchedFilesCriterion), typeof(UnwatchedFilesCriterion) },
         { nameof(EpisodeWithoutFilesCriterion), typeof(EpisodeWithoutFilesCriterion) },
-        { nameof(AnimeTypeCriterion), typeof(AnimeTypeCriterion) }
+        { nameof(AnimeTypeCriterion), typeof(AnimeTypeCriterion) },
+        { nameof(GenericFilesCriterion), typeof(GenericFilesCriterion) }
     };
 
     [Parameter]
@@ -36,6 +37,7 @@ public partial class CriteriaPicker
             { } t when t == typeof(UnwatchedFilesCriterion) => new UnwatchedFilesCriterion(false),
             { } t when t == typeof(EpisodeWithoutFilesCriterion) => new EpisodeWithoutFilesCriterion(false),
             { } t when t == typeof(AnimeTypeCriterion) => new AnimeTypeCriterion(false, AnimeType.TvSeries),
+            { } t when t == typeof(GenericFilesCriterion) => new GenericFilesCriterion(false),
             _ => throw new ArgumentOutOfRangeException()
         };
         if (and?.Criteria.Count > index)
---
+++ Shizou.Data/FilterCriteria/GenericFilesCriterion.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+using Shizou.Data.Models;
+
+namespace Shizou.Data.FilterCriteria;
+
+public record GenericFilesCriterion(bool Negated) : TermCriterion(Negated)
+{
+    protected override Expression<Func<AniDbAnime, bool>> MakeTerm()
+    {
+        return anime => anime.AniDbEpisodes.Any(e => e.AniDbFiles.OfType<AniDbGenericFile>().Any(f => f.LocalFiles.Any()));
+    }
+}
