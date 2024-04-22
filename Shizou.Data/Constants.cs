3984419c49003b2d2b1fdb85fc4b93fb2e243994
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-04-22 06:01:38 -0700

    Add scheme handler download links

--- Shizou.Data/Constants.cs
+++ Shizou.Data/Constants.cs
@@ -5,4 +5,11 @@ public static class Constants
     public const string IdentityCookieName = "IdentityCookie";
     public const string ApiPrefix = "/api";
     public const string AppLockName = "ShizouApp_40FBD174-A80E-4139-8A8E-18BAD1216528";
+    public const string SchemeHandlerVersion = "v1.0.0";
+
+    public const string SchemeHandlerWindowsDownloadUri =
+        $"https://github.com/Mik1ll/Shizou/releases/download/SchemeHandler%2F{SchemeHandlerVersion}/SchemeHandler_Win_x64_{SchemeHandlerVersion}.zip";
+
+    public const string SchemeHandlerLinuxDownloadUri =
+        $"https://github.com/Mik1ll/Shizou/releases/download/SchemeHandler%2F{SchemeHandlerVersion}/SchemeHandler_Linux_x64_{SchemeHandlerVersion}.zip";
 }
