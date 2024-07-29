d1dda49d3575c7ca61a69781f23f27f36ca505bc
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-07-29 02:51:43 -0700

    Load videojs before server start

---
+++ Shizou.Blazor/wwwroot/Shizou.Blazor.lib.module.js
@@ -0,0 +1,7 @@
+﻿// noinspection JSUnusedGlobalSymbols,JSUnusedLocalSymbols
+
+'use strict';
+
+export function beforeServerStart(options, extensions) {
+    import("/lib/video.js/alt/video.novtt.min.js");
+}
