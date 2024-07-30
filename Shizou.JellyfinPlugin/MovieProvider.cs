293ba71b333f470af49bc5e24454cdeed3e3690e
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-07-30 00:27:22 -0700

    Add movie and series providers

---
+++ Shizou.JellyfinPlugin/MovieProvider.cs
@@ -0,0 +1,15 @@
+﻿using MediaBrowser.Controller.Entities.Movies;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Providers;
+
+namespace Shizou.JellyfinPlugin;
+
+public class MovieProvider : IRemoteMetadataProvider<Movie, MovieInfo>
+{
+    public Task<IEnumerable<RemoteSearchResult>> GetSearchResults(MovieInfo searchInfo, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public Task<MetadataResult<Movie>> GetMetadata(MovieInfo info, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public string Name => "Shizou";
+    public Task<HttpResponseMessage> GetImageResponse(string url, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
---
+++ Shizou.JellyfinPlugin/SeriesProvider.cs
@@ -0,0 +1,16 @@
+﻿using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Providers;
+
+namespace Shizou.JellyfinPlugin;
+
+public class SeriesProvider : IRemoteMetadataProvider<Series, SeriesInfo>
+{
+    public Task<IEnumerable<RemoteSearchResult>> GetSearchResults(SeriesInfo searchInfo, CancellationToken cancellationToken) =>
+        throw new NotImplementedException();
+
+    public Task<MetadataResult<Series>> GetMetadata(SeriesInfo info, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public string Name => "Shizou";
+    public Task<HttpResponseMessage> GetImageResponse(string url, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
