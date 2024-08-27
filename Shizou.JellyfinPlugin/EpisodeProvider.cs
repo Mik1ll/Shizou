09fe47ba760caec36e0508486dcdf66e6a1aea00
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-08-27 03:48:53 -0700

    Add EpisodeProvider.cs

---
+++ Shizou.JellyfinPlugin/EpisodeProvider.cs
@@ -0,0 +1,16 @@
+﻿using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Providers;
+
+namespace Shizou.JellyfinPlugin;
+
+public class EpisodeProvider : IRemoteMetadataProvider<Episode, EpisodeInfo>
+{
+    public Task<IEnumerable<RemoteSearchResult>> GetSearchResults(EpisodeInfo searchInfo, CancellationToken cancellationToken) =>
+        throw new NotImplementedException();
+
+    public Task<MetadataResult<Episode>> GetMetadata(EpisodeInfo info, CancellationToken cancellationToken) => throw new NotImplementedException();
+
+    public string Name => "Shizou";
+    public Task<HttpResponseMessage> GetImageResponse(string url, CancellationToken cancellationToken) => throw new NotImplementedException();
+}
