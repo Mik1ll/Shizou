61de095404ca54d4c1a237d5ec9de26a5d37a902
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-09-20 05:21:34 -0700

    Add get anidb ep by file id method

--- Shizou.Server/Controllers/AniDbEpisodes.cs
+++ Shizou.Server/Controllers/AniDbEpisodes.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Shizou.Data;
 using Shizou.Data.Database;
 using Shizou.Data.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Shizou.Server.Controllers;
 
@@ -11,5 +16,15 @@ public class AniDbEpisodes : EntityGetController<AniDbEpisode>
 {
     public AniDbEpisodes(IShizouContext context) : base(context, episode => episode.Id)
     {
+    }
+
+    [HttpGet("[action]/{id}")]
+    [SwaggerResponse(StatusCodes.Status200OK)]
+    [SwaggerResponse(StatusCodes.Status404NotFound)]
+    [Produces("application/json")]
+    public ActionResult<List<AniDbEpisode>> ByAniDbFileId(int id)
+    {
+        var result = DbSet.AsNoTracking().Where(e => e.AniDbEpisodeFileXrefs.Any(xref => xref.AniDbFileId == id)).ToList();
+        return Ok(result);
     }
 }
