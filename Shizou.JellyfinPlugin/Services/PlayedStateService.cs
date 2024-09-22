37b741d20480d17a45d7d0d9f146040e4972dce0
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-09-22 04:25:52 -0700

    Start on watched state syncing

---
+++ Shizou.JellyfinPlugin/Services/PlayedStateService.cs
@@ -0,0 +1,31 @@
+﻿using Jellyfin.Data.Enums;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Session;
+using Microsoft.Extensions.Logging;
+
+namespace Shizou.JellyfinPlugin.Services;
+
+public class PlayedStateService
+{
+    private readonly ILogger<PlayedStateService> _logger;
+    private readonly IUserManager _usermanager;
+    private readonly IUserDataManager _userDataManager;
+    private readonly ISessionManager _sessionManager;
+
+    public PlayedStateService(ILogger<PlayedStateService> logger, IUserManager usermanager, IUserDataManager userDataManager, ISessionManager sessionManager)
+    {
+        _logger = logger;
+        _usermanager = usermanager;
+        _userDataManager = userDataManager;
+        _sessionManager = sessionManager;
+        _logger.LogInformation("Played Service Created");
+    }
+
+    public async Task UpdateStates(CancellationToken cancellationToken)
+    {
+        var fileStates = await Plugin.Instance.ShizouHttpClient.FileWatchedStatesGetAllAsync(cancellationToken).ConfigureAwait(false);
+        var adminUser = _usermanager.Users.First(u => u.HasPermission(PermissionKind.IsAdministrator));
+        var userData = _userDataManager.GetAllUserData(adminUser.Id);
+        ;
+    }
+}
---
+++ Shizou.Server/Controllers/FileWatchedStates.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shizou.Data;
+using Shizou.Data.Database;
+using Shizou.Data.Models;
+
+namespace Shizou.Server.Controllers;
+
+[ApiController]
+[Route($"{Constants.ApiPrefix}/[controller]")]
+public class FileWatchedStates : EntityController<FileWatchedState>
+{
+    public FileWatchedStates(IShizouContext context) : base(context, state => state.AniDbFileId)
+    {
+    }
+}
