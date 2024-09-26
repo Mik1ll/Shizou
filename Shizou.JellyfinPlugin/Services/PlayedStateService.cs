7278ffb91d57d2a331d3c2ccef01a4f7213e978f
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-09-26 02:54:37 -0700

    Implement played state sync task

--- Shizou.JellyfinPlugin/Services/PlayedStateService.cs
+++ Shizou.JellyfinPlugin/Services/PlayedStateService.cs
@@ -1,6 +1,7 @@
 ﻿using Jellyfin.Data.Enums;
+using MediaBrowser.Controller.Entities;
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Controller.Session;
+using MediaBrowser.Model.Entities;
 using Microsoft.Extensions.Logging;
 using Shizou.JellyfinPlugin.Extensions;
 
@@ -11,23 +12,47 @@ public class PlayedStateService
     private readonly ILogger<PlayedStateService> _logger;
     private readonly IUserManager _usermanager;
     private readonly IUserDataManager _userDataManager;
-    private readonly ISessionManager _sessionManager;
+    private readonly ILibraryManager _libraryManager;
 
-    public PlayedStateService(ILogger<PlayedStateService> logger, IUserManager usermanager, IUserDataManager userDataManager, ISessionManager sessionManager)
+    public PlayedStateService(ILogger<PlayedStateService> logger, IUserManager usermanager, IUserDataManager userDataManager, ILibraryManager libraryManager)
     {
         _logger = logger;
         _usermanager = usermanager;
         _userDataManager = userDataManager;
-        _sessionManager = sessionManager;
-        _logger.LogInformation("Played Service Created");
+        _libraryManager = libraryManager;
     }
 
     public async Task UpdateStates(CancellationToken cancellationToken)
     {
-        var fileStates = await Plugin.Instance.ShizouHttpClient.WithLoginRetry(
-            (sc, ct) => sc.FileWatchedStatesGetAllAsync(ct), cancellationToken).ConfigureAwait(false);
+        _logger.LogInformation("Starting watched state sync");
+        var fileStates = (await Plugin.Instance.ShizouHttpClient.WithLoginRetry(
+                (sc, ct) => sc.FileWatchedStatesGetAllAsync(ct), cancellationToken).ConfigureAwait(false))
+            .ToDictionary(fs => fs.AniDbFileId);
         var adminUser = _usermanager.Users.First(u => u.HasPermission(PermissionKind.IsAdministrator));
-        var userData = _userDataManager.GetAllUserData(adminUser.Id);
-        ;
+
+        var videos = _libraryManager.GetItemList(new InternalItemsQuery(adminUser)
+        {
+            MediaTypes = [MediaType.Video],
+            Recursive = true,
+            IsFolder = false,
+            SourceTypes = [SourceType.Library],
+            IsVirtualItem = false,
+            HasAnyProviderId = new Dictionary<string, string>() { { ProviderIds.ShizouEp, string.Empty } }
+        });
+        foreach (var vid in videos)
+        {
+            if (!fileStates.TryGetValue(Convert.ToInt32(vid.ProviderIds[ProviderIds.ShizouEp]), out var fileState))
+                continue;
+            var userDataItem = _userDataManager.GetUserData(adminUser, vid);
+            if (userDataItem.Played != fileState.Watched)
+            {
+                _logger.LogInformation("Found out of sync played state: AniDB file ID: {AniDbFileId}, Jellyfin: {JellyState}, Shizou: {ShizouState}",
+                    fileState.AniDbFileId, userDataItem.Played, fileState.Watched);
+
+                _logger.LogInformation("Setting played state of item: {PlayedState} => {NewPlayedState}", userDataItem.Played, fileState.Watched);
+                userDataItem.Played = fileState.Watched;
+                _userDataManager.SaveUserData(adminUser, vid, userDataItem, UserDataSaveReason.TogglePlayed, cancellationToken);
+            }
+        }
     }
 }
