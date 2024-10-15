af6158ac6ce52c1e77fe778d49f2c9d32dcb6192
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2024-10-15 05:51:54 -0700

    Add creator external id

---
+++ Shizou.JellyfinPlugin/ExternalIds/AniDbCreatorId.cs
@@ -0,0 +1,16 @@
+﻿using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Providers;
+
+namespace Shizou.JellyfinPlugin.ExternalIds;
+
+public class AniDbCreatorId : IExternalId
+{
+    public bool Supports(IHasProviderIds item) => item is Person;
+
+    public string ProviderName => "AniDB";
+    public string Key => ProviderIds.ShizouCreator;
+    public ExternalIdMediaType? Type => ExternalIdMediaType.Person;
+    public string UrlFormatString => "https://anidb.net/creator/{0}";
+}
