a37f6d30e6b052a7c591372c82b23425fcf33e4e
Mik1ll <27949189+Mik1ll@users.noreply.github.com>
2025-03-31 00:48:20 -0700

    Add DB migration for quote replacement

---
+++ Shizou.Data/Migrations/20250331064257_ReplaceAniDbQuotes.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Sqlite.Storage.Internal;
+
+#nullable disable
+
+namespace Shizou.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReplaceAniDbQuotes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("""
+                UPDATE AniDbEpisodes
+                SET TitleEnglish = replace(TitleEnglish, '`', ''''),
+                    TitleTranscription = replace(TitleTranscription, '`', ''''),
+                    TitleOriginal = replace(TitleOriginal, '`', ''''),
+                    Summary = replace(Summary, '`', '''');
+                """);
+            migrationBuilder.Sql("""
+                UPDATE AniDbAnimes
+                SET TitleEngish = replace(TitleEngish, '`', ''''),
+                    TitleTranscription = replace(TitleTranscription, '`', ''''),
+                    TitleOriginal = replace(TitleOriginal, '`', ''''),
+                    Description = replace(Description, '`', ''''),
+                    Tags = replace(Tags, '`', '''');
+                """);
+            migrationBuilder.Sql("""
+                UPDATE AniDbCharacters
+                SET Name = replace(Name, '`', '''');
+                """);
+            migrationBuilder.Sql("""
+                 UPDATE AniDbCreators
+                 SET Name = replace(Name, '`', '''');
+                 """);
+            migrationBuilder.Sql("""
+                UPDATE AniDbGroups
+                SET Name = replace(Name, '`', ''''),
+                    ShortName = replace(ShortName, '`', '''');
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
